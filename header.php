2eb43c4cddcf898be1908f1985bb243a1cfa7d9e
Ngomeli <joshuangomeli@gmail.com>
2021-10-09 08:25:48 +0300

    creating the Admin panel

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/6153223a38.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Noah Distributors</title>
--- login.php
+++ login.php
@@ -51,7 +51,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/6153223a38.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Noah Distributors</title>
