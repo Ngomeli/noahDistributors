5bdd52a66db34f23a1e56938913c5010b3bf5dd2
Ngomeli <joshuangomeli@gmail.com>
2021-10-31 18:32:50 +0300

    able to manage bookings from the admin panel

--- admin/manage-services-booking.php
+++ admin/manage-services-booking.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 include('../dbConnection.php');
 include('partials/header.php');
 
@@ -6,23 +7,111 @@ include('partials/header.php');
 <main>
     <div class="wrapper">
     <h1>Manage Services Booking</h1>
-       <table class="tbl-full">
-           <tr>
-               <th>S.N</th>
-               <th>Full Name</th>
-               <th>Username</th>
-               <th>Actions</th>
-           </tr>
+       <?php 
+        if(isset($_SESSION['update'])){
+            echo $_SESSION['update'];
+            unset($_SESSION['update']);
+            }
+            ?>
+          <br>
+          <table class="tbl-full">
+       <tr>
+                        <th>S.N.</th>
+                        <th>Food</th>
+                        <th>Price</th>
+                        <th>Qty.</th>
+                        <th>Total</th>
+                        <th>Order Date</th>
+                        <th>Status</th>
+                        <th>Customer Name</th>
+                        <th>Contact</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </tr>
 
-           <tr>
-               <td>1.</td>
-               <td>Joshua Ngomeli</td>
-               <td>Ngomeli</td>
-               <td>
-                  <a href="#" class="btn-secondary">Update Admin</a> 
-                  <a href="#" class="btn-danger">Delete Admin</a> 
-               </td>
-           </tr>
+                    <?php 
+                        //Get all the bookings from database
+                        $sql = "SELECT * FROM service_booking ORDER BY id DESC"; // DIsplay the Latest Booking at First
+                        //Execute Query
+                        $res = mysqli_query($conn, $sql);
+                        //Count the Rows
+                        $count = mysqli_num_rows($res);
+
+                        $sn = 1; //Create a Serial Number and set its initail value as 1
+
+                        if($count>0)
+                        {
+                            //Order Available
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                //Get all the order details
+                                $id = $row['id'];
+                                $service = $row['service'];
+                                $price = $row['price'];
+                                $qty = $row['qty'];
+                                $total = $row['total'];
+                                $book_date = $row['book_date'];
+                                $status = $row['status'];
+                                $customer_name = $row['customer_name'];
+                                $customer_contact = $row['customer_contact'];
+                                $customer_email = $row['customer_email'];
+                                $customer_address = $row['customer_address'];
+                                
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $sn++; ?>. </td>
+                                        <td><?php echo $service; ?></td>
+                                        <td><?php echo $price; ?></td>
+                                        <td><?php echo $qty; ?></td>
+                                        <td><?php echo $total; ?></td>
+                                        <td><?php echo $book_date; ?></td>
+                                        <td>
+                                            <?php 
+                                                // Booked, Available, Make Booking, Cancelled
+
+                                                if($status=="Booked")
+                                                {
+                                                    echo "<label>$status</label>";
+                                                }
+                                                elseif($status=="Available")
+                                                {
+                                                    echo "<label style='color: orange;'>$status</label>";
+                                                }
+                                                elseif($status=="Booking Received")
+                                                {
+                                                    echo "<label style='color: green;'>$status</label>";
+                                                }
+                                                elseif($status=="Cancelled")
+                                                {
+                                                    echo "<label style='color: red;'>$status</label>";
+                                                }
+                                            ?>
+                                        </td>
+
+                                        <td><?php echo $customer_name; ?></td>
+                                        <td><?php echo $customer_contact; ?></td>
+                                        <td><?php echo $customer_email; ?></td>
+                                        <td><?php echo $customer_address; ?></td>
+                                        <td>
+                                            <a href="update-booking.php?id=<?php echo $id; ?>" class=" btn btn-secondary">Update Booking</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+
+                            }
+                        }
+                        else
+                        {
+                            //Order not Available
+                            echo "<tr><td colspan='12' class='error'>Bookings not Available</td></tr>";
+                        }
+                    ?>
+
+ 
+               
        </table>
     </div>
 </main>
---
+++ admin/update-booking.php
@@ -0,0 +1,122 @@
+<?php
+    session_start();
+    include('../dbConnection.php');
+    include('partials/header.php');
+    ?>
+<main>
+<div class="container">
+                <div class="row">
+                    <div class="col-2">
+            <div class="formContainerAdd">
+                            <div class="formBtn">
+                                <span>Update Booking</span>
+                                <hr id="indicator">
+                                <?php
+                                //check if id id set or not
+                                if(isset($_GET['id'])){
+                                    $id = $_GET['id'];
+                                    $sql = "SELECT * FROM service_booking WHERE id=$id";
+                                    $res = mysqli_query($conn,$sql);
+                                    //count the rows to if id is valid
+                                    $count = mysqli_num_rows($res);
+                                    if($count==1){
+                                        $row = mysqli_fetch_assoc($res);
+                                        $service = $row['service'];
+                                        $price = $row['price'];
+                                        $qty = $row['qty'];
+                                        $status = $row['status'];
+                                        $customer_name = $row['customer_name'];
+                                        $customer_contact = $row['customer_contact'];
+                                        $customer_email = $row['customer_email'];
+                                        $customer_address= $row['customer_address'];
+                                    }else{
+                                        $_SESSION['no-booking-found']="<div class='error'>Booking not found.</div>";
+                                        header("Location:manage-services-booking.php");
+                                    }
+                                }else{
+                                    header("Location:manage-services-booking.php");
+                                }
+                                // if(isset($_SESSION['add'])){
+                                //     echo $_SESSION['add'];
+                                //     unset($_SESSION['add']);
+                                // }
+                                
+                                ?>
+                            </div>
+                            <form action="" method="POST">
+                            <label>Service Name:</label>
+                           <b> <?php echo $service; ?><br></b>
+                                <label>Price:</label>
+                               <b>Ksh <?php echo $price; ?></b><br>
+                               <label>Qty</label>
+                               <input type="number" name="qty" value="<?php echo $qty; ?>">
+                                <label>Status:
+                                    <select name="status">
+                                    <option <?php if($status=="Booked"){echo "selected";}?> value="Booked">Booked</option>
+                                    <option <?php if($status=="Available"){echo "selected";}?> value="Available">Availabe</option>
+                                    <option <?php if($status=="Booking Recieved"){echo "selected";}?> value="Booking Recieved">Booking Received</option>
+                                    <option <?php if($status=="Cancelled"){echo "selected";}?> value="Cancelled">Cancelled</option>
+                                    </select>
+                                    </label> <br>                          
+                                    <label>Customer Name:</label>
+                               <input type="text" name="customer_name" value="<?php echo $customer_name; ?>">
+                               <label>Customer Contact:</label>
+                               <input type="text" name="customer_contact" value="<?php echo $customer_contact; ?>">
+                               <label>Customer Email:</label>
+                               <input type="text" name="customer_email" value="<?php echo $customer_email; ?>">
+                               <label>Customer Address:</label>
+                               <textarea name="customer_address" cols="30" rows="5"><?php echo $customer_address; ?></textarea>
+                                
+                                <input type="hidden" name="id" value="<?php echo $id;?>">
+                                <input type="hidden" name="price" value="<?php echo $price;?>">
+                                <button type="submit" name="submit" class="btn">Update Booking</button>
+                                   
+                             </form>
+                             <?php
+                            if(isset($_POST['submit'])){
+                                //get all the data
+                                $id = $_POST['id'];
+                                $price = $_POST['price'];
+                                $qty = $_POST['qty'];
+                
+                                $total = $price * $qty;
+                
+                                $status = $_POST['status'];
+                
+                                $customer_name = $_POST['customer_name'];
+                                $customer_contact = $_POST['customer_contact'];
+                                $customer_email = $_POST['customer_email'];
+                                $customer_address = $_POST['customer_address'];
+
+                               
+                                //update the database
+                                $sql2 = "UPDATE service_booking SET
+                                qty = $qty,
+                                total = $total,
+                                status = '$status',
+                                customer_name = '$customer_name',
+                                customer_contact = '$customer_contact',
+                                customer_email = '$customer_email',
+                                customer_address = '$customer_address'
+                                WHERE id=$id
+                                ";
+                                //execute query
+                                $res2 = mysqli_query($conn,$sql2);
+
+                                if($res2==true){
+                                    $_SESSION['update']= "<div class='success'>Booking Updated Successfully.</div>";
+                                    header("Location:manage-services-booking.php");
+                                }else{
+                                    $_SESSION['update']= "<div class='error'>Failed to Update Booking.</div>";
+                                    header("Location:manage-services-booking.php");
+                                }
+                            }
+                             ?>
+            </div>
+                    </div>
+                </div>
+</div>
+ </main>
+
+
+<?php include('partials/footer.php');?>
